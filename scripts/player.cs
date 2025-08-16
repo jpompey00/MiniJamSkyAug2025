089ccb508a714ef1c4af993619803bce8a90a870
Julian Pompey <julianpomp00@gmail.com>
2025-08-16 11:45:41 -0400

    added cusor aiming and player sprites

--- scripts/player.cs
+++ scripts/player.cs
@@ -14,17 +14,21 @@ public partial class player : CharacterBody2D
 	Boolean isOnRightEdge = false;
 	Boolean isOnLeftEdge = false;
 	int positionOnTile;
+	AnimatedSprite2D animatedSprite2D;
+	Shooter shooter;
 
 
 	public override void _Ready()
 	{
 		tileMap = GetNode<TileMap>("../TileMap");
+		animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		shooter = GetNode<Shooter>("Shooter");
 		// GD.Print(tileMap);
-		tiledata = tileMap.GetCellTileData(0, new Vector2I(1, 1));
+		// tiledata = tileMap.GetCellTileData(0, new Vector2I(1, 1));
 
 		//gets the vector2I Position for this, pretty sick
-		GD.Print(Position);
-		GD.Print(tileMap.LocalToMap(Position));
+		// GD.Print(Position);
+		// GD.Print(tileMap.LocalToMap(Position));
 
 	}
 
@@ -65,12 +69,14 @@ public partial class player : CharacterBody2D
 		if (Input.IsActionJustPressed(Controls.Left.ToString()) && !(positionOnTile == (int)PositionOnTile.ON_LEFT_EDGE))
 		{
 			GD.Print("Left Pressed, allowed to move left");
+			// animatedSprite2D.Scale = new Vector2(-1, 1);
 			Position = new Vector2(Position.X - 32, Position.Y);
 
 		}
 		if (Input.IsActionJustPressed(Controls.Right.ToString()) && !(positionOnTile == (int)PositionOnTile.ON_RIGHT_EDGE))
 		{
 			GD.Print("Right Pressed, allowed to move right");
+			// animatedSprite2D.Scale = new Vector2(1, 1);
 			Position = new Vector2(Position.X + 32, Position.Y);
 		}
 		if (Input.IsActionJustPressed(Controls.Throw1.ToString()))
@@ -81,6 +87,19 @@ public partial class player : CharacterBody2D
 		if (Input.IsActionJustPressed(Controls.Throw2.ToString()))
 		{
 			GD.Print("Throw-2 Pressed");
+		}
+
+
+		if ((shooter != null) && shooter.directionToCursor.X < 0)
+		{
+			// GD.Print(shooter.directionToCursor.X);
+			animatedSprite2D.Scale = new Vector2(-1, 1);
+			// GD.Print("face left");
+		}
+		if ((shooter != null) && shooter.directionToCursor.X > 0)
+		{
+			animatedSprite2D.Scale = new Vector2(1, 1);
+			// GD.Print("face right");
 		}
 
 	}
